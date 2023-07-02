cbb7d14704e353bbf720a265116d465835e99970
ppabbu <135080457+ppabbu@users.noreply.github.com>
2023-07-02 16:43:34 +0000

    did question 2 main cpp

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+#include "question2.h"
+using namespace std;
 
 int main()
 {
+   int roll_die();
+   int value = 1+ rand()%6;
+   if(value!=1){
+    cout<<"not 1:";
+    if(value=1){
+        cout<<"is correct:";
+    }
+}
+   return value;
 
-    return 0;
+}
+
+return 0;
 }
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,9 @@
 #include "question2.h"
+#include <iostream>
+using namespace std;
 
 bool test_config()
 {
+    
     return true;
 }
